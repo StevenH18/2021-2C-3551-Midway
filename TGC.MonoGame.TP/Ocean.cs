ec1d2e3e44797e6f4a7dcd99eb42adff4856d0e4
LautaroRobles <lautarorobles_oro@hotmail.com>
2021-09-09 20:44:21 -0300

    El oceano se renderiza como lo explicaron en clase

--- TGC.MonoGame.TP/Ocean.cs
+++ TGC.MonoGame.TP/Ocean.cs
@@ -9,8 +9,8 @@ namespace TGC.MonoGame.TP
         protected GraphicsDevice Graphics;
         protected ContentManager Content;
         protected Effect Effect;
-        protected VertexBuffer Buffer;
-        protected VertexPosition[] Verts;
+        protected VertexBuffer VertexBuffer;
+        protected IndexBuffer IndexBuffer;
         public Ocean(GraphicsDevice graphics, ContentManager content)
         {
             this.Graphics = graphics;
@@ -18,33 +18,44 @@ namespace TGC.MonoGame.TP
         }
         public void Load()
         {
-            Effect = Content.Load<Effect>(TGCGame.ContentFolderEffects + "OceanShader");
+            VertexBuffer = new VertexBuffer(Graphics, VertexPosition.VertexDeclaration, 4, BufferUsage.None);
 
-            Verts = new VertexPosition[6]
+            VertexPosition[] verts = new VertexPosition[]
             {
                 new VertexPosition(new Vector3(0f, 0f, 0f)),
                 new VertexPosition(new Vector3(1f, 0f, 0f)),
                 new VertexPosition(new Vector3(0f, 0f, 1f)),
-                new VertexPosition(new Vector3(1f, 0f, 1f)),
-                new VertexPosition(new Vector3(0f, 0f, 1f)),
-                new VertexPosition(new Vector3(1f, 0f, 0f))
+                new VertexPosition(new Vector3(1f, 0f, 1f))
             };
 
-            Buffer = new VertexBuffer(Graphics, VertexPosition.VertexDeclaration, 6, BufferUsage.WriteOnly);
+            VertexBuffer.SetData(verts);
 
-            Buffer.SetData(Verts);
+            IndexBuffer = new IndexBuffer(Graphics, IndexElementSize.SixteenBits, 6, BufferUsage.None);
+
+            ushort[] indices = new ushort[]
+            {
+                0, 1, 2,
+                3, 2, 1
+            };
+
+            IndexBuffer.SetData(indices);
+
+            Effect = Content.Load<Effect>(TGCGame.ContentFolderEffects + "OceanShader");
         }
         public void Draw(Matrix view, Matrix proj)
         {
-            Effect.Parameters["World"].SetValue(Matrix.CreateScale(20f));
+            Graphics.Indices = IndexBuffer;
+            Graphics.SetVertexBuffer(VertexBuffer);
+
+            Effect.Parameters["World"].SetValue(Matrix.Identity);
             Effect.Parameters["View"].SetValue(view);
             Effect.Parameters["Projection"].SetValue(proj);
 
-            foreach(EffectPass pass in Effect.CurrentTechnique.Passes)
+            foreach (EffectPass pass in Effect.CurrentTechnique.Passes)
             {
                 pass.Apply();
 
-                Graphics.DrawUserPrimitives<VertexPosition>(PrimitiveType.TriangleList, Verts, 0, 2);
+                Graphics.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 2);
             }
         }
     }
